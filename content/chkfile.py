c50467336c696982abcd1f384357591df08b8fc3
gilbertliou <gilbert.liou.tw@gmail.com>
2015-10-08 14:50:57 +0800

    add twcom_tej

---
+++ content/chkfile.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from os.path import exists
+from datetime import datetime
+import time
+from traceback import print_exc
+import re
+from pdb import set_trace
+
+
+def gett(fi):
+    try:
+        return time.ctime(os.path.getmtime(fi))
+    except:
+        return 0
+
+
+srcpath = '/home/gilbert/repos/rating/result/'
+
+
+def watch(fis, dstpaths):
+    chk = lambda mainfi, srcpath, dstpath, fidic: [
+        chkfi(srcpath, dstpath, fi, fidic) for fi in reads(mainfi)]
+
+    t0 = {fi: gett(fi) for fi in fis}
+    fidics = {}
+    for fi, dstpath in zip(fis, dstpaths):
+        fidics[fi] = {}
+        chk(fi, srcpath, dstpath, fidics[fi])
+
+    def chking(fi, dstpath):
+        t1 = gett(fi)
+        if t1 != t0[fi]:
+            print datetime.now().strftime('%H:%M:%S')
+            try:
+                chk(fi, srcpath, dstpath, fidics[fi])
+                os.chdir('..')
+                os.system('make html')
+                os.chdir('content')
+                t0[fi] = t1
+            except:
+                print_exc()
+
+    while 1:
+        map(chking, fis, dstpaths)
+        time.sleep(1)
+
+
+def reads(fi):
+    keys = [
+        re.compile('\{filename\}/?([\/\w\.]+)'),
+        ]
+
+    while 1:
+        try:
+            f = open(fi)
+            break
+        except:
+            print_exc()
+            time.sleep(1)
+
+    for li in f:
+        for key in keys:
+            x = key.search(li)
+            if x:
+                yield x.groups()[0]
+    f.close()
+
+
+def chkfi(srcpath, dstpath, fi, fidic):
+    fi1 = fi.replace(dstpath, '')
+    src = srcpath + fi1
+    t1 = gett(src)
+
+    if not exists(fi):
+        cp(src, dstpath)
+        fidic[src] = t1
+
+    if src not in fidic:
+        fidic[src] = gett(fi)
+        # fidic[src] = t1
+    if fidic[src] != t1:
+        cp(src, dstpath)
+        fidic[src] = t1
+
+
+def cp(src, dstpath):
+    try:
+        os.system('cp %s %s' % (src, dstpath))
+        print src, 'copied'
+    except:
+        print 'ERROR COPY:', src
+        print_exc()
+        set_trace()
+
+
+if __name__ == '__main__':
+    mainfis = [
+        '2015-10-ivstSNA.md',
+        # '2015-09-kmt_history.md',
+        '2015-10-twcom_tej.md',
+        ]
+    paths = ['ivstSNA', 'twcom_tej']
+    dstpaths = [('images/%s_files/' % fi) for fi in paths]
+    watch(mainfis, dstpaths)
+
---
+++ content/draft/2015-09-kmt_history.md
@@ -0,0 +1,55 @@
+title: 從上市櫃公司看黨營事業變化 
+date: 2015-09-24 17:23
+comments: true
+slug: kmt_history
+tags: 台灣公司關係圖
+category: 台灣公司關係圖
+
+從之前的文章中，
+我們發現過去黨營事業在不少上市櫃公司擔任董監事，
+因此本篇文章即將討論黨營事業的投資變化。
+
+下圖為黨營事業的直接持股變化
+
+[add pic]
+
+其中中央投資、光華投資等為國民黨完全持股的黨營事業，
+其在 2002 年以前直接投資公司家數一度位居全國前三名。
+
+但我們接下來想問的是，
+若包含各種間接投資的話，
+黨營事業究竟會持有多少公司？
+為此我製作了持股動畫來表示黨營事業的持股變化。
+以表格方式來表達的話，
+我們可以看到核心成員、非核心成員與投資夥伴的分別。
+
+[add table]
+
+其中我們可以看到，在近幾年國民黨擔任董監事的公司數大幅減少，至 2007 年已沒有任何相關資訊了。但真的是這樣嗎？
+
+從之前的文章知道，
+黨營事業依然在不少公司佔有董監事席次，
+但這些公司均非上市櫃公司，
+所以至今僅能從經濟部營利事業登記資料與國民黨的公開年報得知大略的投資內容了。
+這件事其實很可惜，不論任何政黨或政治人物，
+因其主要從事的工作影響公眾利益甚鉅，
+為降低利益衝突的可能性，
+理應公佈財產內容供大眾審閱監督。
+但若有方便的機器讀取格式，
+就能讓民間更易於發展相關應用。
+只是這點對當前政府而言，
+應該是相當不情願的吧。
+
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img
+alt="Creative Commons License" style="border-width:0"
+src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span
+xmlns:dct="http://purl.org/dc/terms/"
+property="dct:title">Gilbert's Data Lab</span> by <a
+xmlns:cc="http://creativecommons.org/ns#" href="http://WeiChengLiou.github.io/"
+property="cc:attributionName" rel="cc:attributionURL">WeiCheng Liou</a> is
+licensed under a <a rel="license"
+href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons
+Attribution-ShareAlike 4.0 International License</a>.<br />Based on a work at <a
+xmlns:dct="http://purl.org/dc/terms/" href="http://WeiChengLiou.github.io/"
+rel="dct:source">http://WeiChengLiou.github.io/</a>.
